084b478a5dc035489c2a4d6c08a86ebfa7681d91
Marcus <mefchristiansen@gmail.com>
2020-06-28 14:42:04 -0400

    update gitignore

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 YOUTUBE_AUTH_PICKLE = "youtube_auth.pickle"
-YOUTUBE_CLIENT_SECRETS = "youtube_client_secret.json"
+YOUTUBE_CLIENT_SECRETS = "youtube_client_secrets.json"
 YOUTUBE_SECRETS = "youtube_secrets.json"
 
 SPOTIFY_AUTH_PICKLE = "spotify_auth.pickle"
