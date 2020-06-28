1412626640ecd8bc3d283dbce52d1e279643891d
Marcus <mefchristiansen@gmail.com>
2020-06-28 14:59:59 -0400

    update file paths

--- constants.py
+++ constants.py
@@ -1,7 +1,10 @@
-YOUTUBE_AUTH_PICKLE = "youtube_auth.pickle"
-YOUTUBE_CLIENT_SECRETS = "youtube_client_secrets.json"
-YOUTUBE_SECRETS = "youtube_secrets.json"
+import os
 
-SPOTIFY_AUTH_PICKLE = "spotify_auth.pickle"
-SPOTIFY_CLIENT_SECRETS = "spotify_client_secrets.json"
-SPOTIFY_SECRETS = "spotify_secrets.json"
+SPOTIFY_AUTH_PICKLE = os.path.join("secrets", "pickles", "spotify_auth.pickle")
+YOUTUBE_AUTH_PICKLE = os.path.join("secrets", "pickles", "youtube_auth.pickle")
+
+SPOTIFY_CLIENT_SECRETS = os.path.join("secrets", "client_secrets", "spotify_client_secrets.json")
+YOUTUBE_CLIENT_SECRETS = os.path.join("secrets", "client_secrets", "youtube_client_secrets.json")
+
+SPOTIFY_SECRETS = os.path.join("secrets", "secrets", "spotify_secrets.json")
+YOUTUBE_SECRETS = os.path.join("secrets", "secrets", "youtube_secrets.json")
